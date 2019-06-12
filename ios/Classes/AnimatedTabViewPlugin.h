80d8af582215a0a87034cfcaa3bf864d488040f9
mporeda <michal.poreda@consileon.pl>
2019-06-12 16:14:34 +0200

    pods fixed

---
+++ ios/Classes/AnimatedTabViewPlugin.h
@@ -0,0 +1,4 @@
+#import <Flutter/Flutter.h>
+
+@interface AnimatedTabViewPlugin : NSObject<FlutterPlugin>
+@end
---
+++ ios/Classes/AnimatedTabViewPlugin.m
@@ -0,0 +1,20 @@
+#import "AnimatedTabViewPlugin.h"
+
+@implementation AnimatedTabViewPlugin
++ (void)registerWithRegistrar:(NSObject<FlutterPluginRegistrar>*)registrar {
+  FlutterMethodChannel* channel = [FlutterMethodChannel
+      methodChannelWithName:@"animated_tab_view"
+            binaryMessenger:[registrar messenger]];
+  AnimatedTabViewPlugin* instance = [[AnimatedTabViewPlugin alloc] init];
+  [registrar addMethodCallDelegate:instance channel:channel];
+}
+
+- (void)handleMethodCall:(FlutterMethodCall*)call result:(FlutterResult)result {
+  if ([@"getPlatformVersion" isEqualToString:call.method]) {
+    result([@"iOS " stringByAppendingString:[[UIDevice currentDevice] systemVersion]]);
+  } else {
+    result(FlutterMethodNotImplemented);
+  }
+}
+
+@end
